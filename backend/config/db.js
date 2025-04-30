ed077ff00aa262efeb2d5ce48d5d7a8b439e1efd
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-05-01 03:50:59 +0530

    Update MongoDB connection string to use Atlas cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/bloggy");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+    try {
+        const conn = await mongoose.connect("mongodb+srv://kshitiz:RdqHHBQZg0ClmNSa@blogapp.bu7qrmt.mongodb.net/");
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
